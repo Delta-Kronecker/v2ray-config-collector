1a71eef6b576c1917496f119be5250e8f1bcc55d
Ahmad <diracdeltain0@gmail.com>
2025-09-22 20:40:22 +0330

    Update proxy-tester.go

--- xray_test/proxy-tester.go
+++ xray_test/proxy-tester.go
@@ -75,9 +75,9 @@ func NewDefaultConfig() *Config {
 
 	return &Config{
 		XrayPath:        getEnvOrDefault("XRAY_PATH", ""),
-		MaxWorkers:      getEnvIntOrDefault("PROXY_MAX_WORKERS", 1000),
+		MaxWorkers:      getEnvIntOrDefault("PROXY_MAX_WORKERS", 500),
 		Timeout:         time.Duration(getEnvIntOrDefault("PROXY_TIMEOUT", 5)) * time.Second,
-		BatchSize:       getEnvIntOrDefault("PROXY_BATCH_SIZE", 1000),
+		BatchSize:       getEnvIntOrDefault("PROXY_BATCH_SIZE", 500),
 		IncrementalSave: getEnvBoolOrDefault("PROXY_INCREMENTAL_SAVE", true),
 		DataDir:         dataDir,
 		ConfigDir:       configDir,
